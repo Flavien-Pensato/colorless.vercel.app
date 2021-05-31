78619a6ae4b36021a6914806835e86a5e8b93839
Flavien-Pensato <flavien.pensato@gmail.com>
2021-06-01 00:06:09 +0200

    resolve #8 - create grid panel of colors from one color

--- packages/core/src/__test__/index.spec.ts
+++ packages/core/src/__test__/index.spec.ts
@@ -1,4 +1,4 @@
-import {HextoRGB, RGBtoHEX, shade, tint} from "../colorless";
+import { HextoRGB, RGBtoHEX, shade, tint, toGrid } from "../colorless";
 
 describe("HextoRGB()", () => {
   it("should return rgb", () => {
@@ -9,7 +9,6 @@ describe("HextoRGB()", () => {
     expect(HextoRGB("ffffff")).toEqual([255, 255, 255]);
   });
 });
-
 
 describe("RGBtoHEX()", () => {
   it("should return hexa", () => {
@@ -31,5 +30,22 @@ describe("tint()", () => {
     expect(tint(0, 0, 0, 0)).toEqual("#000000");
     expect(tint(0, 0, 0, 0.5)).toEqual("#7F7F7F");
     expect(tint(0, 0, 0, 1)).toEqual("#FFFFFF");
+  });
+});
+
+describe("toGrid()", () => {
+  it("should get grid for #ED8936", () => {
+    expect(toGrid("#ED8936")).toEqual({
+      "50": "#FDF3EA",
+      "100": "#FBE7D6",
+      "200": "#F7CFAE",
+      "300": "#F4B886",
+      "400": "#F0A05E",
+      "500": "#ED8936",
+      "600": "#BD6D2B",
+      "700": "#8E5220",
+      "800": "#5E3615",
+      "900": "#2F1B0A"
+    });
   });
 });
