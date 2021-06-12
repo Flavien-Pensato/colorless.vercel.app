b84563c0360c43194122e0d44906a25f3c097b74
Flavien-Pensato <flavien.pensato@gmail.com>
2021-06-12 20:11:02 +0200

    feat(): add ga

--- packages/app/pages/_app.js
+++ packages/app/pages/_app.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
+import ReactGA from 'react-ga'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  ReactGA.initialize('UA-46017345-5')
+
   return (
     <ChakraProvider>
       <Component {...pageProps} />
--- packages/app/pages/index.js
+++ packages/app/pages/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import Head from 'next/head'
+import ReactGA from 'react-ga'
 import { LandingTemplate } from '../templates'
 
 export default function Home() {
+  ReactGA.pageview('/')
+
   return (
     <div>
       <Head>
